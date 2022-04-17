daa0c18fd38c8c0a8cb2c6eae768259474330d26
devshakti78 <shakti.dev46@gmail.com>
2022-04-17 17:46:34 +0530

    able to show the teacher list

--- Client/final-countdown/src/main.jsx
+++ Client/final-countdown/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    {/* <Provider> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </Provider> */}
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
