08821725a8c2bfcac160886a07ca333f75fc5186
devshakti78 <shakti.dev46@gmail.com>
2022-04-17 13:28:54 +0530

    relationship-established

--- Server/src/configs/db.js
+++ Server/src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://shakti:shakti@cluster0.ycerw.mongodb.net/ReduxTeachers",
+    "mongodb://shakti:shakti@cluster0-shard-00-00.ycerw.mongodb.net:27017,cluster0-shard-00-01.ycerw.mongodb.net:27017,cluster0-shard-00-02.ycerw.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-bun363-shard-0&authSource=admin&retryWrites=true&w=majority",
   );
 };
